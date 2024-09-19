224679fe5e0ca1bfa858b0bba11065cc3def04fb
ShivamAttri842 <shivamattri.842@gmail.com>
2024-09-19 18:50:55 +0530

    Updated Day - 3

--- Day -3/JavaBasics01.java
+++ Day -3/JavaBasics01.java
@@ -1,23 +1,32 @@
-public class JavaBasics01{
+public class JavaBasics01 {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
+
+        // Addition Assignment
+        System.out.println("Initial a = " + a);
         a += b;
-        System.out.println("a = " + a);
+        System.out.println("After a += b, a = " + a);
 
         a = 5;
-        b = 10;
+
+        // Subtraction Assignment
+        System.out.println("\nInitial a = " + a);
         a -= b;
-        System.out.println("a = " + a);
-        
-        a = 5;
-        b = 10;
-        a *= b;
-        System.out.println("a = " + a);
+        System.out.println("After a -= b, a = " + a);
 
         a = 5;
-        b = 10;
+
+        // Multiplication Assignment
+        System.out.println("\nInitial a = " + a);
+        a *= b;
+        System.out.println("After a *= b, a = " + a);
+
+        a = 5;
+
+        // Division Assignment
+        System.out.println("\nInitial a = " + a);
         a /= b;
-        System.out.println("a = " + a);
+        System.out.println("After a /= b, a = " + a);
     }
 }
