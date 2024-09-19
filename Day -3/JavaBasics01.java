dcfaf114da8ef9baf4c6c6255b69db7e47453a1c
ShivamAttri842 <shivamattri.842@gmail.com>
2024-09-19 18:29:42 +0530

    Added Day-3

---
+++ Day -3/JavaBasics01.java
@@ -0,0 +1,23 @@
+public class JavaBasics01{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        a += b;
+        System.out.println("a = " + a);
+
+        a = 5;
+        b = 10;
+        a -= b;
+        System.out.println("a = " + a);
+        
+        a = 5;
+        b = 10;
+        a *= b;
+        System.out.println("a = " + a);
+
+        a = 5;
+        b = 10;
+        a /= b;
+        System.out.println("a = " + a);
+    }
+}
