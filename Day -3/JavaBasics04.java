f17f9e9fe7eaadea14030ba6e9a70dd4aa0f4791
ShivamAttri842 <shivamattri.842@gmail.com>
2024-09-19 18:42:24 +0530

    Updated Day - 3

---
+++ Day -3/JavaBasics04.java
@@ -0,0 +1,16 @@
+public class JavaBasics04 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        // Logical AND (&&)
+        System.out.println("x && y: " + (x && y)); // false
+
+        // Logical OR (||)
+        System.out.println("x || y: " + (x || y)); // true
+
+        // Logical NOT (!)
+        System.out.println("!x: " + (!x)); // false
+        System.out.println("!y: " + (!y)); // true
+    }
+}
