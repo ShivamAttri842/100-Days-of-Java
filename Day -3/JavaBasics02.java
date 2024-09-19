7d1bae3240ee81bf860ca37276bfc5eea30ca2c1
ShivamAttri842 <shivamattri.842@gmail.com>
2024-09-19 18:39:57 +0530

    Added Day - 3

---
+++ Day -3/JavaBasics02.java
@@ -0,0 +1,8 @@
+public class JavaBasics02 {
+    public static void main(String[] args) {
+        int a = 23;
+        System.out.println("The original value of a is "+ a);
+        int b = ++a;
+        System.out.println("The new value of a is "+ b);
+    }
+}
