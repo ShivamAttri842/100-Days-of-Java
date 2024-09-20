576db14df086476ccbb3e2a5bc9edb7b71822749
ShivamAttri842 <shivamattri.842@gmail.com>
2024-09-20 19:20:58 +0530

    Updated Day - 4

---
+++ Day - 4/JavaBasics08.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class JavaBasics08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Menu:");
+        System.out.println("1. Pizza - $10");
+        System.out.println("2. Burger - $5");
+        System.out.println("3. Pasta - $8");
+        System.out.println("4. Salad - $6");
+
+        System.out.print("Please select an item (1-4): ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You ordered Pizza. The price is $10.");
+                break;
+            case 2:
+                System.out.println("You ordered Burger. The price is $5.");
+                break;
+            case 3:
+                System.out.println("You ordered Pasta. The price is $8.");
+                break;
+            case 4:
+                System.out.println("You ordered Salad. The price is $6.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid menu item.");
+        }
+
+        scanner.close();
+    }
+}
